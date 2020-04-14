b089d2245d604cc83baa4456c267f98c47e44844
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2020-04-14 00:01:54 -0300

    add model schema to item and change resource to use this

--- resources/item.py
+++ resources/item.py
@@ -1,30 +1,21 @@
 
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
+from flask import request
 from flask_jwt_extended import jwt_required
 from models.item import ItemModel
+from marshmallow import ValidationError
+from schemas.item import ItemSchema
 
+item_schema = ItemSchema()
+item_list_schema = ItemSchema(many=True)
 
 class Item(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'price',
-        type=float,
-        required=True,
-        help="esse campo é obrigario"
-    )
-    parser.add_argument(
-        'store_id',
-        type=int,
-        required=True,
-        help="esse campo é obrigario"
-    )
 
     @classmethod
     def get(cls, name: str):
         item = ItemModel.find_by_name(name)
         if item:
-            return item.json()
+            return item_schema.dump(item)
         return {"message": "Item not found"}, 404
 
     @classmethod
@@ -32,13 +23,18 @@ class Item(Resource):
     def post(cls, name: str):
         if ItemModel.find_by_name(name):
             return {"message": "Item already exists"}, 400
-        data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'], data['store_id'])
+        
+        item_json = request.get_json()
+        item_json["name"] = name
+        try:
+            item = item_schema.load(item_json)
+        except ValidationError as err:
+            return err.messages, 400
         try:
             item.save_to_db()
         except:
             return {"message": "ocorreu um erro"}, 500
-        return item.json(), 201
+        return item_schema.dump(item), 201
 
     @classmethod
     @jwt_required
@@ -52,22 +48,23 @@ class Item(Resource):
     @classmethod
     @jwt_required
     def put(cls, name: str):
-        data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
+        item_json = request.get_json()
         if item:
-            item.price = data['price']
-            item.store_id = data['store_id']
-
+            item.price = item_json["price"]
             item.save_to_db()
             return {"message": "updated"}, 200
-
-        item = ItemModel(name, **data)
+        item_json["name"] = name
+        try:
+            item = item_schema.load(item_json)
+        except ValidationError as err:
+            return err.messages, 400
         item.save_to_db()
-        return {"message": "created"}, 200
+        return item_schema.dump(item), 200
 
 
 class ItemList(Resource):
 
     @classmethod
     def get(cls):
-        return {"items": list(item.json() for item in ItemModel.find_all())}
+        return {"items": list(item_list_schema.dump(ItemModel.find_all()))}
