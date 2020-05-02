13d042d0da1315be97d09830d5f413d2a5e54c82
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2020-05-02 18:47:25 -0300

    finalize image helper

--- image_helper.py
+++ image_helper.py
@@ -17,19 +17,17 @@ def get_path(filename=None, folder=None):
 
 def find_image_any_format(filename, folder):
     for _format in IMAGES:
-        image =f"{filename}.{format}"
+        image = f"{filename}.{format}"
         image_path = IMAGE_SET.path(filename=image, folder=folder)
         if os.path.isfile(image_path):
             return image_path
     return None
 
 
-
 def _retrieve_file_name(file):
     if isinstance(file, FileStorage):
         return file.filename
     return file
-    
 
 
 def is_file_name_safe(file):
@@ -37,11 +35,14 @@ def is_file_name_safe(file):
 
     allowed_format = "|".join(IMAGES)
     regex = f"^[a-zA-Z0-9][a-zA-Z0-9_()-\.]*\.({allowed_format})$"
-    return re.match(regex, filename)
-
-def get_basebame():
-    pass
+    return re.match(regex, filename) is not None
 
 
-def get_extension():
-    pass
+def get_basename(file):
+    filename = _retrieve_file_name(file)
+    return os.path.split(filename)[1]
+
+
+def get_extension(file):
+    filename = _retrieve_file_name(file)
+    return os.path.splitext(filename)[1]
