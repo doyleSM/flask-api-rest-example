c20ffe3b6c71e78080becca7050867f4c8adeaab
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2020-05-03 16:56:04 -0300

    add download image from user

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogo
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload
+from resources.image import ImageUpload, Image
 from libs.image_helper import IMAGE_SET
 from blacklist import BLACKLIST
 
@@ -53,6 +53,7 @@ api.add_resource(UserLogout, '/logout')
 api.add_resource(Confirmation, '/user_confirmation/<string:confirmation_id>')
 api.add_resource(ConfirmationByUser, '/confirmation/user/<int:user_id>')
 api.add_resource(ImageUpload, '/upload/image' )
+api.add_resource(Image, '/image/<string:filename>' )
 
 if __name__ == '__main__':
     db.init_app(app)
